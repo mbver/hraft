b902e2b8f6a384e316e752649b100d7fd55dd604
phuoc <phuocdongxoai@gmail.com>
2024-12-29 23:06:56 +0700

    membership

--- membership.go
+++ membership.go
@@ -1,3 +1,14 @@
 package hraft
 
-type membership struct{}
+type peerRole int8
+
+type peer struct {
+	id   string
+	role peerRole
+}
+type membership struct {
+	latest       []*peer
+	lastestIdx   uint64
+	commited     []*peer
+	committedIdx uint64
+}
